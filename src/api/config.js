e6619e6fd2c3e13b945c76823d205e16799c5c13
Cobbler <417663733@qq.com>
2018-07-30 14:05:29 +0800

    https

--- src/api/config.js
+++ src/api/config.js
@@ -3,7 +3,7 @@ export const CODE_ERR = 110;
 export const SECRET_KEY = 'WAPMAN';
 /* eslint-disable */
 export const HOST_API = process.env.NODE_ENV === 'production' ?
-  'http://www.jsvue.cn:3000' : 'http://localhost:5000';
+  'https://www.jsvue.cn/api' : 'http://localhost:5000';
 export const WX_HOST = process.env.NODE_ENV === 'production' ?
   'http://wx.pjpiao.com' : 'http://wxt.pjpiao.com';
 /* eslint-enable */
